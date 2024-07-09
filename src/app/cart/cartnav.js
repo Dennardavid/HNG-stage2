118b01a622e5bd862e0715434f8d3b4dcc3a4000
Dennar David <dennardavid@gmail.com>
2024-07-09 22:48:09 +0100

    mobile

--- src/app/cart/cartnav.js
+++ src/app/cart/cartnav.js
@@ -1,8 +1,13 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import "../globals.css";
 
 function CartNavbar({ heading }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="px-[100px] py-[15px] border-b-[0.5px] border-b-[#BEC0BF]">
       <nav className="flex items-center justify-between">
@@ -12,23 +17,70 @@ function CartNavbar({ heading }) {
 
         <h1 className="text-[35px] font-medium text-[#00140D]">{heading}</h1>
         <div className="flex items-center w-[250px] justify-between">
-          <div className="flex">
-            <Image src="/profile.svg" width={36} height={36} />
-            <div className="flex justify-center items-center ml-2 cursor-pointer">
+          <div className="flex  items-center w-[300px] justify-between">
+            <div
+              className="flex justify-center items-center ml-2 cursor-pointer gap-3 relative"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <div>
+                <Image
+                  src="https://res.cloudinary.com/dsngkhlct/image/upload/v1720540388/profile_owx8pe.svg"
+                  width={36}
+                  height={36}
+                />
+              </div>
               <p>Hi, Ebunoluwa</p>
-              <Image src="/Navigation.svg" width={24} height={24} />
-            </div>
-          </div>
+              <span>
+                {isOpen ? (
+                  <Image
+                    src="https://res.cloudinary.com/dsngkhlct/image/upload/v1720540372/Navigation2_ok6uhp.svg"
+                    width={24}
+                    height={24}
+                  />
+                ) : (
+                  <Image
+                    src="https://res.cloudinary.com/dsngkhlct/image/upload/v1720540370/Navigation_scinpl.svg"
+                    width={24}
+                    height={24}
+                  />
+                )}
+              </span>
 
-          <div>
-            <Link href="/cart">
-              <Image
-                src="/cart.svg"
-                width={24}
-                height={24}
-                className="cursor-pointer"
-              />
-            </Link>
+              {/* Drop down menu */}
+              {isOpen && (
+                <div className="absolute top-12 right-2 w-[150px] bg-[#F2F1F0] rounded-md shadow-md flex flex-col gap-2 z-20">
+                  <Link
+                    href={"/"}
+                    className="hover:bg-[#FF7E08] p-2 hover:text-[#f2f1f0] rounded-t-md"
+                  >
+                    Explore
+                  </Link>
+                  <Link
+                    href={"/cart"}
+                    className="hover:bg-[#FF7E08] p-2 hover:text-[#f2f1f0]"
+                  >
+                    Cart
+                  </Link>
+                  <Link
+                    href={"/"}
+                    className="hover:bg-[#FF7E08] p-2 hover:text-[#f2f1f0] rounded-b-md"
+                  >
+                    Wishlist
+                  </Link>
+                </div>
+              )}
+            </div>
+
+            {/* Cart  */}
+            <div>
+              <Link href="/cart">
+                <Image
+                  src="https://res.cloudinary.com/dsngkhlct/image/upload/v1720540360/cart_jesv3q.svg"
+                  width={24}
+                  height={24}
+                />
+              </Link>
+            </div>
           </div>
         </div>
       </nav>
